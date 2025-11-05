4a9b782495423822234710dbbcd071322cd0664c
Martin Jul <martin.henriksen@3mj.dk>
2025-11-05 21:56:37 +0100

    Fleshed out Payment class, and started on Sale

--- DigitalKasseSystem/DigitalKasseSystem/Payment.cs
+++ DigitalKasseSystem/DigitalKasseSystem/Payment.cs
@@ -8,5 +8,19 @@ namespace DigitalKasseSystem
 {
     class Payment
     {
+        double amount;
+        PaymentMethod paymentMethod;
+
+        // Constructor for choosing payment method
+        public Payment(PaymentMethod paymentMethod)
+        {
+            this.paymentMethod = paymentMethod;
+        }
+
+        // Method to update the amount to be paid
+        public void UpdateAmount(double amount) // Maybe needs to return something later
+        {
+            this.amount = amount;
+        }
     }
 }
--- DigitalKasseSystem/DigitalKasseSystem/Sale.cs
+++ DigitalKasseSystem/DigitalKasseSystem/Sale.cs
@@ -8,5 +8,45 @@ namespace DigitalKasseSystem
 {
     class Sale
     {
+        public int SaleNumber { get; }
+        public Payment Payment;
+        DateTime startTime;
+        DateTime endTime;
+        List<Item> basket;
+        double total;
+
+        // Constructor for Sale class, starting a new sale
+        public Sale(int saleNumber)
+        {
+            SaleNumber = saleNumber;
+            startTime = DateTime.Now;
+            basket = new List<Item>();
+            total = 0.0;
+        }
+
+        // Method to end the sale
+        public void EndSale()
+        {
+            endTime = DateTime.Now;
+        }
+
+        // Methods to add items from the basket
+        public void AddToBasket(string itemNumber)
+        {
+            // To be implemented
+            // Need Item Repository to get Item by itemNumber
+        }
+
+        // Methods to remove items from the basket
+        public void RemoveFromBasket(string itemNumber)
+        {
+            // To be implemented
+            // Need Item Repository to get Item by itemNumber
+        }
+
+        public override string ToString() // Change later for saving functionality
+        {
+            return base.ToString();
+        }
     }
 }
