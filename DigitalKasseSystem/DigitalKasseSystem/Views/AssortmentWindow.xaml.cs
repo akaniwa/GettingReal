9df2618df8cf58ae200728d3d034cbfd61d0e998
Martin Jul <martin.henriksen@3mj.dk>
2025-11-26 12:21:20 +0100

    Assortment window xaml made

--- DigitalKasseSystem/DigitalKasseSystem/Views/AssortmentWindow.xaml.cs
+++ DigitalKasseSystem/DigitalKasseSystem/Views/AssortmentWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace DigitalKasseSystem.Views
         {
             InitializeComponent();
         }
+
+        private void ReturnButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
